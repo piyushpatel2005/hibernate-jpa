cf3359c2735f8725295d0434aaed3e7767d70527
Piyush Patel <piyushpatel2005@gmail.com>
2018-05-19 16:24:11 -0400

    Get unique objects from join

--- CourseManagement/src/com/piyushpatel2005/testharness/HibernateTestHarness.java
+++ CourseManagement/src/com/piyushpatel2005/testharness/HibernateTestHarness.java
@@ -25,34 +25,10 @@ public class HibernateTestHarness
 		tx.begin();
 		
 		// let's do some queries!
-		TypedQuery<Student> q = em.createQuery("from Student as student where student.address.city = :name", Student.class);
-		
-		q.setParameter("name", "Georgia");
-		
-		List<Student> allStudents = q.getResultList();
-		for(Student student: allStudents)  {
-			System.out.println(student);
-		}
-		
-		TypedQuery<Tutor> tutorQuery = em.createQuery("from Tutor as tutor where tutor.supervisionGroup is empty", Tutor.class);
-		List<Tutor> tutors = tutorQuery.getResultList();
-		for(Tutor t: tutors) {
-			System.out.println(t);
-		}
-		
-		tutorQuery = em.createQuery("from Tutor as tutor where tutor.supervisionGroup is not empty", Tutor.class);
-		tutors = tutorQuery.getResultList();
-		for(Tutor t: tutors) {
-			System.out.println(t);
-		}
-		
-		Subject science = em.find(Subject.class, 2);
-		// list of all students whose supervisor can teach science
-		Query scienceQuery = em.createQuery("from Student as student where :subject member of student.supervisor.subjectsQualifiedToTeach");
-		scienceQuery.setParameter("subject", science);
-		List<Student> scienceTutors = scienceQuery.getResultList();
-		for(Student t: scienceTutors) {
-			System.out.println(t);
+		Query q = em.createQuery("select distinct tutor from Tutor as tutor join tutor.supervisionGroup as student where student.address.city='Georgia'");
+		List<Tutor> results = q.getResultList();
+		for(Tutor next: results) {
+			System.out.println(next);
 		}
 		
 		tx.commit();
@@ -87,13 +63,14 @@ public class HibernateTestHarness
 		// This tutor is the only tutor who can teach History
 		Tutor t3 = new Tutor("GHI678", "Linda Histroia", 0);
 		t3.addSubjectToQualifications(history);
+//		t3.addSubjectToQualifications(science);
 		
 		em.persist(t1);
 		em.persist(t2);
 		em.persist(t3);
 
 		// this only works because we are cascading from tutor to student
-		t1.createStudentAndAddToSupervisionGroup("Marco Fortes", "1-FOR-2010", "1 The Street", "Anytown", "484848");
+		t1.createStudentAndAddToSupervisionGroup("Marco Fortes", "1-FOR-2010", "1 The Street", "Georgia", "484848");
 		t1.createStudentAndAddToSupervisionGroup("Kath Grainer", "2-GRA-2009", "2 Kaths Street", "Georgia", "939393");
 		t3.createStudentAndAddToSupervisionGroup("Sandra Perkins", "3-PER-2009", "4 The Avenue", "Georgia", "939393");
 		
